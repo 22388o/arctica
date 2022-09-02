3bc8759ee7b3840e665f2c51004487ae8f3d2d80
lappy <wildkard91@gmail.com>
2022-09-02 14:08:11 -0400

    added changes to make bitcoin dotfile function

--- src/main.rs
+++ src/main.rs
@@ -49,11 +49,29 @@ fn test_function() -> String {
 	format!("completed with no problems")
 }
 
+//front-end: boot
+// runs on the boot screen when user clicks install, downloads latest copy of tails
 #[tauri::command]
 fn obtain_tails() -> String {
 	println!("fetching the latest copy of tails");
 	let output = Command::new("wget")
             .args(["--continue", "http://dl.amnesia.boum.org/tails/stable/tails-amd64-5.4/tails-amd64-5.4.img"])
+            .output()
+            .expect("failed to execute process");
+    for byte in output.stdout {
+    	print!("{}", byte as char);
+    }
+    println!(";");
+	format!("completed with no problems")
+}
+
+//front-end: setup 1
+//create the bitcoin dotfile on the local machine internal disk, where block data will be stored 
+#[tauri::command]
+fn make_bitcoin_dotfile() -> String {
+	println!("Making Bitcoin dotfile");
+	let output = Command::new("mkdir")
+			.args(["--parents", "/home/$USER/.bitcoin/chainstate", ";", "mkdir", "/home/$USER/.bitcoin/blocks"])
             .output()
             .expect("failed to execute process");
     for byte in output.stdout {
@@ -79,20 +97,6 @@ fn create_bootable_usb() -> String {
 	//"printf '%s\n' n y g y | mksub ~/arctica/resources/ubunntu-22.04-desktop-amd64.iso"
 	//"kvm -m 2048 -hdb /dev/sda -boot d -cdrom ~/arctica/resources/ubuntu-22.04-deskotp-amd64.iso"
   	// println!("I was invoked from JS, with this message: {}, {}", invoke_message, height);
-}
-
-#[tauri::command]
-fn make_bitcoin_dotfile() -> String {
-	println!("Making Bitcoin dotfile");
-	let output = Command::new("bash")
-			.args(["./scripts/makebitcoindotfile.sh"])
-            .output()
-            .expect("failed to execute process");
-    for byte in output.stdout {
-    	print!("{}", byte as char);
-    }
-    println!(";");
-	format!("completed with no problems")
 }
 
 #[tauri::command]
