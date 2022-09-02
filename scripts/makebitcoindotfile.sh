bff4a2ffdc468fc995387d0ccb6d55da6aa3332d
lappy <wildkard91@gmail.com>
2022-09-02 15:09:08 -0400

    removed unnecessary test data from bash script file

--- scripts/makebitcoindotfile.sh
+++ scripts/makebitcoindotfile.sh
@@ -2,31 +2,3 @@
 #currently issues with this not creating the sub directories...
 echo "running makebitcoindotfile"
 sudo mkdir --parents /home/$USER/.bitcoin/blocks /home/$USER/.bitcoin/chainstate
-
-# if [[ -d ../../../.bitcoin ]]
-# then
-#     echo "dotfile already exists"
-# else
-# then
-#     echo "creating dotfile"
-#     mkdir -parents ../../../.bitcoin/chainstate && mkdir blocks ../../../.bitcoin/
-#     exit
-# fi
-
-# if [[ -d ../../../.bitcoin/chainstate ]]
-# then
-#     echo "chainstate already exists"
-# else
-# then
-#     echo "creating chainstate"
-#     mkdir chainstate ../../../.bitcoin/
-# fi
-
-# if [[ -d ../../../.bitcoin/blocks ]]
-# then
-#     echo "blocks already exists"
-# else
-# then
-#     echo "creating blocks"
-#     mkdir blocks ../../../.bitcoin 
-# fi
