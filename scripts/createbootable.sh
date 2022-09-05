4104b793ab19907f69b4b7385a419a8c416dc06c
lappy <wildkard91@gmail.com>
2022-09-05 11:10:29 -0400

    added a script which handles the initial flash for SD 1-7

--- scripts/createbootable.sh
+++ scripts/createbootable.sh
@@ -1,3 +1,15 @@
 #this script will execute when the user clicks the 'continue' button on each of the first setup screens for all 7 SD cards
 
-echo "running initial flash"
+target=$(ls /dev/sd?)
+
+len=$(echo -n "$target" | wc -c)
+
+if [[ $len -lt 8 ]]
+then
+echo no target device found, try again
+elif [[ $len -lt 9 ]]
+then
+sudo dd if=tails-amd64-5.4.img of="$target" bs=16M oflag=direct status=progress
+else
+echo more than one target device found, confused about what to do
+fi
