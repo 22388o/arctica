e996d656743e5df46d7e19856e5318a7dc593f8f
lappy <wildkard91@gmail.com>
2022-10-12 21:57:31 -0400

    added notes to init-iso

--- scripts/init-iso.sh
+++ scripts/init-iso.sh
@@ -1,4 +1,12 @@
+#download KVM deps
 sudo apt-get -y install qemu-system-x86 qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils
+
+#download mkusb deps
+sudo add-apt-repository -y universe
+sudo add-apt-repository -y ppa:mkusb/ppa
+sudo apt -y update
+sudo apt install -y mkusb
+sudo apt install -y usb-pack-efi
 
 FILE="./ubuntu-22.04.1-desktop-amd64.iso"
 FILE1="./bitcoin-23.0-x86_64-linux-gnu.tar.gz"
@@ -15,10 +23,12 @@ fi
 sudo rm persistent-ubuntu.iso
 sudo rm persistent-ubuntu1.iso
 sudo rm pid.txt
+#modify ubuntu iso to have persistence
 < ubuntu-22.04.1-desktop-amd64.iso sed 's/maybe-ubiquity/  persistent  /' > persistent-ubuntu1.iso
 < persistent-ubuntu1.iso sed 's/set timeout=30/set timeout=1 /' > persistent-ubuntu.iso
 sudo rm persistent-ubuntu1.iso
 fallocate -l 5GiB persistent-ubuntu.iso
+#first time iso boot to establish persistence
 kvm -m 2048 ~/arctica/persistent-ubuntu.iso -daemonize -pidfile pid.txt -cpu host -display none
 sleep 200
 kill -9 $(cat ./pid.txt)
@@ -33,15 +43,11 @@ sudo chmod +x /media/$USER/writable/upper/usr/share/applications/Arctica.desktop
 sudo tar -xzf bitcoin-23.0-x86_64-linux-gnu.tar.gz -C /media/$USER/writable/upper/home/ubuntu
 #make local internal bitcoin dotfile
 sudo mkdir --parents /home/$USER/.bitcoin/blocks /home/$USER/.bitcoin/chainstate
-#create device .bitcoin dir
+#create target device .bitcoin dir
 sudo mkdir /media/$USER/writable/upper/home/ubuntu/.bitcoin
 #symlink chainstate
 sudo ln -s /media/ubuntu/$UUID/home/$USER/.bitcoin/chainstate /media/$USER/writable/upper/home/ubuntu/.bitcoin/chainstate 
 #symlink blockdata
 sudo ln -s /media/ubuntu/$UUID/home/$USER/.bitcoin/blocks /media/$USER/writable/upper/home/ubuntu/.bitcoin/blocks
-#download mkusb deps
-sudo add-apt-repository -y universe
-sudo add-apt-repository -y ppa:mkusb/ppa
-sudo apt -y update
-sudo apt install -y mkusb
-sudo apt install -y usb-pack-efi
+
+
