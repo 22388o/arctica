c0ccf894cbdbffd3e6635a24a07dad187a9f6935
lappy <wildkard91@gmail.com>
2022-10-14 14:37:56 -0400

    changed mount point for internal drive

--- scripts/mount-internal.sh
+++ scripts/mount-internal.sh
@@ -1,8 +1,8 @@
 #mount internal drive
-# sudo mount /dev/nvme0n1p2 /media/ubuntu 
+sudo mount /dev/nvme0n1p2 /media/ubuntu/InternalDisk
 # #remove stale symlinks
 # sudo unlink ~/.bitcoin/chainstate
 # sudo unlink ~/.bitcoin/blocks
 # #create symlinks for chainstate and blockdata
-# ln -s /media/ubuntu/home/$USER/.bitcoin/chainstate ~/.bitcoin/chainstate
-# ln -s /media/ubuntu/home/$USER/.bitcoin/blocks ~/.bitcoin/blocks
+# ln -s /media/ubuntu/InternalDisk/home/$USER/.bitcoin/chainstate ~/.bitcoin/chainstate
+# ln -s /media/ubuntu/InternalDisk/home/$USER/.bitcoin/blocks ~/.bitcoin/blocks
