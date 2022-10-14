ade2ec88a7b4beb73d6f398dd316346dfc56ed73
lappy <wildkard91@gmail.com>
2022-10-14 10:14:45 -0400

    fixed mount point for internal, removed sudo from symbolic links for testing

--- scripts/mount-internal.sh
+++ scripts/mount-internal.sh
@@ -1,8 +1,8 @@
 #mount internal drive
-sudo mount nvme0n1p2 /media/ubuntu
+sudo mount /dev/nvme0n1p2 /media/ubuntu
 #remove stale symlinks
 sudo unlink ~/.bitcoin/chainstate
 sudo unlink ~/.bitcoin/blocks
 #create symlinks for chainstate and blockdata
-sudo ln -s /media/ubuntu/home/$USER/.bitcoin/chainstate ~/.bitcoin/chainstate
-sudo ln -s /media/ubuntu/home/$USER/.bitcoin/blocks ~/.bitcoin/blocks
+ln -s /media/ubuntu/home/$USER/.bitcoin/chainstate ~/.bitcoin/chainstate
+ln -s /media/ubuntu/home/$USER/.bitcoin/blocks ~/.bitcoin/blocks
