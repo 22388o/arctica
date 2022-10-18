7735db835886ba30937508d2449f44adcf6655e9
lappy <wildkard91@gmail.com>
2022-10-18 15:38:29 -0400

    latest

--- scripts/mount-internal.sh
+++ scripts/mount-internal.sh
@@ -1,8 +1,8 @@
 #mount internal drive
 sudo mount /dev/nvme0n1p2 /media/ubuntu
 # #remove stale symlinks
-sudo unlink ~/.bitcoin/chainstate
-sudo unlink ~/.bitcoin/blocks
+# sudo unlink ~/.bitcoin/chainstate
+# sudo unlink ~/.bitcoin/blocks
 # #create symlinks for chainstate and blockdata
 
 #these symlinks are currently broken because $USER is dynamic.
