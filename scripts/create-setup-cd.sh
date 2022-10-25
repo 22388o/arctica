73ab3a5e0f7ae494d89ef6cf7ae8921a113dbf45
wild-kard <wildkard91@gmail.com>
2022-10-25 10:40:59 -0400

    create setup cd script now installs ssss deps, creates masterkey for encryption, and shards masterkey into a 5 of 11 scheme

--- scripts/create-setup-cd.sh
+++ scripts/create-setup-cd.sh
@@ -4,6 +4,8 @@ sudo apt update
 
 #download wodim
 sudo apt install -y wodim
+#download shamir secret sharing
+sudo apt install ssss
 
 #find cd path
 OUTPUT=$(echo $(ls /dev/sr?))
@@ -16,7 +18,11 @@ sudo cp /home/$USER/config.txt /mnt/ramdisk/setupCD
 sudo rm /home/$USER/config.txt
 
 #generate SSH key for encrypting persistent directories and store in setupCD
-ssh-keygen -t rsa -N '' -b 4096 -C "your_email@example.com" -f /mnt/ramdisk/setupCD/ssh_key
+ssh-keygen -t rsa -N '' -b 4096 -C "your_email@example.com" -f /mnt/ramdisk/setupCD/masterkey
+#split masterkey used for encryption into a 5 of 11 scheme
+ssss-split -t 5 -n 11 < /mnt/ramdisk/setupCD/masterkey > /mnt/ramdisk/setupCD/shards.txt
+sudo cp /mnt/ramdisk/masterkey /mnt/ramdisk/setupCD
+sudo cp /mnt/ramdisk/shards.txt /mnt/ramdisk/setupCD
 
 #create iso from setupCD dir
 genisoimage -r -J -o /mnt/ramdisk/setupCD.iso /mnt/ramdisk/setupCD
