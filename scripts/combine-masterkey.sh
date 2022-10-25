c7b59a929ee841f3ea17d844ec197101efb81752
wild-kard <wildkard91@gmail.com>
2022-10-25 13:16:19 -0400

    combine masterkey now correctly pipes stdout to a masterkey file

--- scripts/combine-masterkey.sh
+++ scripts/combine-masterkey.sh
@@ -1,2 +1,2 @@
 #combine atleast 5 key shards inside of shards.txt to retrieve masterkey
-ssss-combine -t 5 < /mnt/ramdisk/shards.txt > /mnt/ramdisk/masterkey
+ssss-combine -t 5 < /mnt/ramdisk/shards.txt 2> /mnt/ramdisk/masterkey.txt
