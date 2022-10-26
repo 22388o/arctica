2185afa669e9619d77902e2a3bceec5684373e17
wild-kard <wildkard91@gmail.com>
2022-10-25 21:54:29 -0400

    shards are now iterated over and split into 11 seperate txt files

--- scripts/create-setup-cd.sh
+++ scripts/create-setup-cd.sh
@@ -18,13 +18,23 @@ sudo cp /home/$USER/config.txt /mnt/ramdisk/setupCD
 sudo rm /home/$USER/config.txt
 
 #generate masterkey for encrypting persistent directories and store in setupCD
-base64 /dev/urandom | head -c 50 > /mnt/ramdisk/setupCD/masterkey
+base64 /dev/urandom | head -c 50 > /mnt/ramdisk/masterkey
 
 #split masterkey used for encryption into a 5 of 11 scheme
-ssss-split -t 5 -n 11 < /mnt/ramdisk/setupCD/masterkey > /mnt/ramdisk/shards_untrimmed.txt
+ssss-split -t 5 -n 11 < /mnt/ramdisk/masterkey > /mnt/ramdisk/shards_untrimmed.txt
 
 #trim excess from the output of ssss split
-sed -e '1d' /mnt/ramdisk/shards_untrimmed.txt > /mnt/ramdisk/setupCD/shards.txt
+sed -e '1d' /mnt/ramdisk/shards_untrimmed.txt > /mnt/ramdisk/shards.txt
+FILE="/mnt/ramdisk/shards.txt"
+Lines=$(cat $FILE)
+X=1
+declare -i X
+for Line in $Lines
+do
+    echo $Line > /mnt/ramdisk/shards/shard$X.txt
+    X+=1
+done
+
 
 sudo cp /mnt/ramdisk/masterkey /mnt/ramdisk/setupCD
 sudo cp /mnt/ramdisk/shards.txt /mnt/ramdisk/setupCD
