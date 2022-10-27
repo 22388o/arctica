48b5ba95453278911f40b4f1975d7625a36305ac
wild-kard <wildkard91@gmail.com>
2022-10-27 16:04:31 -0400

    change memory allocation for ramfs to 100M

--- scripts/create-ramdisk.sh
+++ scripts/create-ramdisk.sh
@@ -1,5 +1,5 @@
 sudo mkdir /mnt/ramdisk
-sudo mount -t ramfs -o size=1G ramfs /mnt/ramdisk
+sudo mount -t ramfs -o size=100M ramfs /mnt/ramdisk
 sudo chmod 777 /mnt/ramdisk
 
 #make target dir for encrypted payload to or from SD cards
