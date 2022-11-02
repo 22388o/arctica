95b9f22a825ac6215f04bea2ce818700b297100d
wild-kard <wildkard91@gmail.com>
2022-11-01 22:22:12 -0400

    added a parents flag to mkddir for create wallet, update latest front end

--- scripts/create-wallet.sh
+++ scripts/create-wallet.sh
@@ -3,7 +3,7 @@ echo "this is a simulated xpriv" > /mnt/ramdisk/sensitive/xpriv.txt
 #create a simulated xpub
 echo "this is a simulated xpub" > /mnt/ramdisk/sensitive/xpub.txt
 
-mkdir /mnt/ramdisk/setupCD/xpubs
+mkdir --parents /mnt/ramdisk/setupCD/xpubs
 
 #export xpub to setupCD
 cp /mnt/ramdisk/sensitive/xpub.txt /mnt/ramdisk/setupCD/xpubs/
