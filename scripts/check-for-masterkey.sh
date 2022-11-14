7921747a6526ddebddb2b8f3c313ea7a723f9545
wild-kard <wildkard91@gmail.com>
2022-11-14 14:45:42 -0500

    checking for masterkey now removes the sensitive dir if it finds a masterkey, also added a collect shards script

--- scripts/check-for-masterkey.sh
+++ scripts/check-for-masterkey.sh
@@ -1,6 +1,8 @@
 FILE="/mnt/ramdisk/masterkey"
 
 if [ -f "$FILE" ]; then 
+    #remove sensitive dir to be unpacked on next step
+    sudo rm -r /mnt/ramdisk/sensitive
     echo "masterkey found"
 else
     echo "no key found"
