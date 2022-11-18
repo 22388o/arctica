0723ce8d724fc25daf7e7f9505e19421fc3b73e8
wild-kard <wildkard91@gmail.com>
2022-11-18 11:40:34 -0500

    fixed a bug with create recovery cd script

---
+++ scripts/create-recovery-cd.sh
@@ -0,0 +1,24 @@
+#find cd path
+OUTPUT=$(echo $(ls /dev/sr?))
+
+#make the transfer CD dir which holds files to be burned to the transfer CD
+mkdir /mnt/ramdisk/transferCD
+
+#create transferCD config
+echo "type=recoverycd" > /mnt/ramdisk/transferCD/config.txt
+
+#collect shards from sd card for export to transfer CD
+cp -r /home/$USER/shards/* /mnt/ramdisk/transferCD/shards
+
+#create iso from transferCD dir
+genisoimage -r -J -o /mnt/ramdisk/transferCD.iso /mnt/ramdisk/transferCD
+
+#wipe the CD
+sudo umount $OUTPUT
+wodim -v dev=$OUTPUT blank=fast
+
+#burn transferCD iso to the transfer CD
+wodim dev=$OUTPUT -v -data /mnt/ramdisk/transferCD.iso
+
+#eject the disk to refresh the filesystem
+eject $OUTPUT
