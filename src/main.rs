970a7847b8bcdbd69924a8ce20fa6a2fae8003f0
wild-kard <wildkard91@gmail.com>
2023-01-06 16:06:11 -0500

    fixed compile errors, initial setup should now properly creates and stores descriptors. still need testing

--- src/main.rs
+++ src/main.rs
@@ -275,7 +275,7 @@ async fn recover_key_pair() -> String {
 	format!("SUCCESS recovered Private/Public Key Pair")
 }
 
-fn build_high_descriptor(blockchain: &RpcBlockchain, keys: Vec<String>) -> Result<String, bdk::Error> {
+fn build_high_descriptor(blockchain: &RpcBlockchain, keys: &Vec<String>) -> Result<String, bdk::Error> {
 	let four_years = blockchain.get_height().unwrap()+210379;
 	let month = 4382;
 	let desc = format!("wsh(and_v(v:thresh(5,pk({}),s:pk({}),s:pk({}),s:pk({}),s:pk({}),s:pk({}),s:pk({}),sun:after({}),sun:after({}),sun:after({}),sun:after({})),thresh(2,pk({}),s:pk({}),s:pk({}),s:pk({}),sun:after({}),sun:after({}))))", keys[0], keys[1], keys[2], keys[3], keys[4], keys[5], keys[6], four_years, four_years+(month), four_years+(month*2), four_years+(month*3), keys[7], keys[8], keys[9], keys[10], four_years, four_years);
@@ -283,14 +283,14 @@ fn build_high_descriptor(blockchain: &RpcBlockchain, keys: Vec<String>) -> Resul
 	Ok(miniscript::Descriptor::<bitcoin::PublicKey>::from_str(&desc).unwrap().to_string())
 }
 
-fn build_med_descriptor(blockchain: &RpcBlockchain, keys: Vec<String>) -> Result<String, bdk::Error> {
+fn build_med_descriptor(blockchain: &RpcBlockchain, keys: &Vec<String>) -> Result<String, bdk::Error> {
 	let four_years = blockchain.get_height().unwrap()+210379;
 	let desc = format!("wsh(thresh(2,pk({}),s:pk({}),s:pk({}),s:pk({}),s:pk({}),s:pk({}),s:pk({}),sun:after({})))", keys[0], keys[1], keys[2], keys[3], keys[4], keys[5], keys[6], four_years);
 	Ok(miniscript::Descriptor::<bitcoin::PublicKey>::from_str(&desc).unwrap().to_string())
 }
 
 
-fn build_low_descriptor(blockchain: &RpcBlockchain, keys: Vec<String>) -> Result<String, bdk::Error> {
+fn build_low_descriptor(blockchain: &RpcBlockchain, keys: &Vec<String>) -> Result<String, bdk::Error> {
 	let desc = format!("wsh(c:or_i(pk_k({}),or_i(pk_h({}),or_i(pk_h({}),or_i(pk_h({}),or_i(pk_h({}),or_i(pk_h({}),pk_h({}))))))))", keys[0], keys[1], keys[2], keys[3], keys[4], keys[5], keys[6]);
 	Ok(miniscript::Descriptor::<bitcoin::PublicKey>::from_str(&desc).unwrap().to_string())
 }
@@ -1013,7 +1013,7 @@ async fn distribute_shards_sd7() -> String {
 }
 
 #[tauri::command]
-async fn create_descriptor(state: State<'_,TauriState>) -> String {
+async fn create_descriptor(state: State<'_, TauriState>) -> Result<String, String> {
 	println!("creating descriptors from 7 xpubs & 4 time machine keys");
 	//convert all 11 public_keys in the ramdisk to an array vector
 	let mut key_array = Vec::new();
@@ -1031,25 +1031,25 @@ async fn create_descriptor(state: State<'_,TauriState>) -> String {
 
 	let blockchain = RpcBlockchain::from_config(&*state.0.lock().unwrap()).expect("failed to connect to bitcoin core(Ensure bitcoin core is running before calling this function)");
 	//build the high security descriptor
-	let high_descriptor = build_high_descriptor(&blockchain, key_array).expect("Failed to build high level descriptor");
+	let high_descriptor = build_high_descriptor(&blockchain, &key_array).expect("Failed to build high level descriptor");
 	let high_file_dest = "/mnt/ramdisk/sensitive/high_descriptor".to_string();
 	//store the high security descriptor in the sensitive dir
 	store_descriptor(high_descriptor, high_file_dest);
 
 	//build the med security descriptor
-	let med_descriptor = build_med_descriptor(&blockchain, key_array).expect("Failed to build high level descriptor");
+	let med_descriptor = build_med_descriptor(&blockchain, &key_array).expect("Failed to build high level descriptor");
 	let med_file_dest = "/mnt/ramdisk/sensitive/med_descriptor".to_string();
 	//store the med security descriptor in the sensitive dir
 	store_descriptor(med_descriptor, med_file_dest);
 
 	//build the low security descriptor
-	let low_descriptor = build_low_descriptor(&blockchain, key_array).expect("Failed to build high level descriptor");
+	let low_descriptor = build_low_descriptor(&blockchain, &key_array).expect("Failed to build high level descriptor");
 	let low_file_dest = "/mnt/ramdisk/sensitive/low_descriptor".to_string();
 	//store the low security descriptor in the sensitive dir
 	store_descriptor(low_descriptor, low_file_dest);
 
 
-	format!("SUCCESS in creating descriptors")
+	Ok(format!("SUCCESS in creating descriptors"))
 
 }
 
