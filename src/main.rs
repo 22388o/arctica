a615f1a02700a2da8cb5ed612b0c27980658572e
wild-kard <wildkard91@gmail.com>
2023-01-11 11:43:50 -0500

    fixed a bug where writable would not be created by init_iso after being umounted from usb stick

--- src/main.rs
+++ src/main.rs
@@ -339,9 +339,12 @@ async fn test_function() -> String {
 async fn init_iso() -> String {
 	println!("obtaining & creating modified ubuntu iso");
 
-	println!("unmounting stale writable and unbuntu mounts if appropriate");
+	println!("removing stale writable");
+	//remove writable if exists, developer failsafe
+	Command::new("sudo").args(["rm", "-r", "-f", &("/media/".to_string()+&get_user()+"/writable")]).output().unwrap();
+
+	println!("unmounting stale unbuntu mount if appropriate");
 	//remove stale mount points if user has started arctica before
-	Command::new("sudo").args(["umount", &("/media/".to_string()+&get_user()+"/writable")]).output().unwrap();
 	Command::new("sudo").args(["umount", &("/media/".to_string()+&get_user()+"/Ubuntu 22.04.1 LTS amd64")]).output().unwrap();
 
 
