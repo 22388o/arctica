3e976a2972d876c4fbc260672d49148fbe95ff55
wild-kard <wildkard91@gmail.com>
2023-01-17 13:02:21 -0500

    fixed a compile error in the previous commit

--- src/main.rs
+++ src/main.rs
@@ -715,7 +715,7 @@ async fn create_setup_cd() -> String {
 async fn copy_cd_to_ramdisk() -> String {
 	//mount CD if not auto mounted
 	Command::new("sudo").args(["mkdir", &("/media/".to_string()+&get_user()+"/CDROM")]).output().unwrap();
-	Command::new("sudo").args(["mount", "/dev/sr0", &("/media/".to_string()+&get_user()+"/CDROM")])
+	Command::new("sudo").args(["mount", "/dev/sr0", &("/media/".to_string()+&get_user()+"/CDROM")]).output().unwrap();
 	//copy cd contents to ramdisk
 	let output = Command::new("cp").args(["-R", &("/media/".to_string()+&get_user()+"/CDROM"), "/mnt/ramdisk"]).output().unwrap();
 	if !output.status.success() {
